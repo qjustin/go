c7ed7111bf25082be1dfb90e7ae96a112f043661
jun.hu <jun.hu@picahealth.com>
2019-02-01 15:48:50 +0800

    changes

---
+++ src/projects/thegolanguage/ch2/tempconv/tempconv.go
@@ -0,0 +1,60 @@
+package tempconv
+
+import "fmt"
+
+/*
+	类型
+	即使类型的底层数据结构相同，它们仍是两个不同的类型。
+
+	每个类型T都有一个对应的类型转换操作T(x)，用于将x转为T类型
+	如果T是指针类型，可能会需要用小括弧包装T，比如(*int)(0),
+	转换的前提是：只有当两个类型的底层基础类型相同时，才允许这种转型操作
+
+	将x类型转换成T类型的时候，x类型的值不变
+
+*/
+type Celsius float64    // 摄氏温度
+type Fahrenheit float64 // 华氏温度
+
+const (
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC     Celsius = 0       // 结冰点温度
+	BoilingC      Celsius = 100     // 沸水温度
+)
+
+func PrintTemp() {
+	// 数值类型之间也可以相互转换
+	fmt.Printf("%g\n", BoilingC-FreezingC) // "100" °C
+	// 将一个浮点数转为整数将丢弃小数部分，BoilingC的值不会被改变
+	boilingF := CToF(BoilingC)
+	// 底层数据类型相同因此可以做减法
+	fmt.Printf("%g\n", boilingF-CToF(FreezingC)) // "180" °F
+	// 底层数据类型不同因此不能做算数运算， 这里报错
+	// fmt.Printf("%g\n", boilingF-FreezingC) // compile error: type mismatch
+
+	// 如果类型底层数据类型相同可以使用==和< 或者类型的底层数据类型与未命名的值类型相同也可以进行比较。
+	// 总之一句话，相同类型才能比较，不管是自定义类型，还是值类型，只要底层类型与值类型的类型相同则可以比较。
+	var c Celsius
+	var f Fahrenheit
+	fmt.Println(c == 0) // "true"
+	fmt.Println(f >= 0) // "true"
+	//fmt.Println(c == f)          // compile error: type mismatch
+	fmt.Println(c == Celsius(f)) // "true"!
+
+	// 许多类型都会定义一个String方法，因为当使用fmt包的打印方法时，将会优先使用该类型对应的String方法返回的结果打印
+	d := FToC(212.0)
+	//fmt.Println(d.String()) // "100°C"
+	fmt.Printf("%v\n", d)   // "100°C"; no need to call String explicitly
+	fmt.Printf("%s\n", d)   // "100°C"
+	fmt.Println(d)          // "100°C"
+	fmt.Printf("%g\n", d)   // "100"; does not call String
+	fmt.Println(float64(d)) // "100"; does not call String
+}
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
---
+++ src/projects/thegolanguage/ch3/demo02/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+/*
+	iota 常量生成器
+	常量声明可以使用iota常量生成器初始化，它用于生成一组以相似规则初始化的常量，但是不用每行都写一遍初始化表达式。
+*/
+
+type Weekday int
+
+const (
+	// 在第一个声明的常量所在的行，iota将会被置为0，然后在每一个有常量声明的行加1
+	// 周日将对应0，周一为1，
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+type Flags uint
+
+const (
+	// 随着iota的递增，每个常量对应表达式1 << iota，是连续的2的幂，分别对应一个bit位置。使用这些常量可以用于测试、设置或清除对应的bit位的值：
+	FlagUp           Flags = 1 << iota // is up
+	FlagBroadcast                      // supports broadcast access capability
+	FlagLoopback                       // is a loopback interface
+	FlagPointToPoint                   // belongs to a point-to-point link
+	FlagMulticast                      // supports multicast access capability
+)
+
+const (
+	_   = 1 << (10 * iota)
+	KiB // 1024
+	MiB // 1048576
+	GiB // 1073741824
+	TiB // 1099511627776             (exceeds 1 << 32)
+	PiB // 1125899906842624
+	EiB // 1152921504606846976
+	ZiB // 1180591620717411303424    (exceeds 1 << 64)
+	YiB // 1208925819614629174706176
+)
+
+func IsUp(v Flags) bool     { return v&FlagUp == FlagUp }
+func TurnDown(v *Flags)     { *v &^= FlagUp }
+func SetBroadcast(v *Flags) { *v |= FlagBroadcast }
+func IsCast(v Flags) bool   { return v&(FlagBroadcast|FlagMulticast) != 0 }
+
+func main() {
+	var v Flags = FlagMulticast | FlagUp
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10001 true"
+	TurnDown(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10000 false"
+	SetBroadcast(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))   // "10010 false"
+	fmt.Printf("%b %t\n", v, IsCast(v)) // "10010 true"
+}
