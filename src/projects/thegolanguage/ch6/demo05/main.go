547b2b6c4ccb4969c3bb465b21231e7678334fcc
hujun <jun.hu@picahealth.com>
2019-02-16 22:15:19 +0800

    changes

--- src/projects/thegolanguage/ch6/demo05/main.go
+++ src/projects/thegolanguage/ch6/demo05/main.go
@@ -1,22 +1,72 @@
-
-
 package main
 
 import (
 	"fmt"
+	"bytes"
 )
 
-type Point struct{ X, Y float64 }
-
 /*
-6.6. 封装
+	6.5. 示例: Bit数组
+	1. 集合用map[T]bool类型来表示虽然非常灵活，但对于数据流分析领域，
+		集合元素通常是一个非负整数，集合会包含很多元素，并且集合会经常
+		进行并集、交集操作，这种情况下，bit数组会比map表现更加理想。
 
-一个对象的变量或者方法如果对调用方是不可见的话，一般就被定义为“封装”。
-
-
-Go语言只有一种控制可见性的手段：大写首字母的标识符会从定义它们的包中被导出，小写字母的则不会。
-这种限制包内成员的方式同样适用于struct或者一个类型的方法。
+	bit集合表示方式：
+		一个bit数组通常会用一个无符号数或者称之为“字”的slice来表示，每一个元素的每一位都表示集合里的一个值。
 */
+
+type IntSet struct {
+	// 用无符号数的slice来，表示bit数组
+	words []uint64
+}
+
 func main() {
 	fmt.Println("hello world")
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
